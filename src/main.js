fa54afcd9b243218a2e806a08c3eea0998708428
Gilang Himawan <gilangx573@gmail.com>
2025-05-14 08:26:51 +0700

    rev2

--- src/main.js
+++ src/main.js
@@ -4,11 +4,3 @@ import './tampilan.css';
 document.addEventListener('DOMContentLoaded', () => {
   initRouter(); 
 });
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then((reg) => console.log('SW registered: ', reg.scope))
-      .catch((err) => console.error('SW registration failed: ', err));
-  });
-}
